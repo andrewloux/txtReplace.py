c400bef0663bedbda4c77d9df7cb6d4721794136
burkean <nothing_to_see_here@example.com>
2013-03-08 20:55:57 +0000

    import re; use re.subn to clean up open->replace

--- txtReplace.py
+++ txtReplace.py
@@ -1,4 +1,4 @@
-import sys, os, glob, argparse, fnmatch
+import sys, os, re, glob, argparse, fnmatch
 
 parser = argparse.ArgumentParser(description='Mass single argument text file find and replace.')
 parser.add_argument('-s','--source', help='Folder containing source text files.', required=True)
@@ -6,27 +6,18 @@ parser.add_argument('-f','--find', help='Find argument', required=True)
 parser.add_argument('-r','--replace', help='Replace argument', required=True)
 args = parser.parse_args()
 
-occurences = 0
- 
 textfiles = []
 for root, dirnames, filenames in os.walk(args.source):
   for filename in fnmatch.filter(filenames, '*.txt'):
       textfiles.append(os.path.join(root, filename))
 
+args.find, args.replace = map(re.escape, [args.find, args.replace])
 for textfile in textfiles:
-	f = open(textfile,'r')
-	contents = f.read()
-	count = contents.count(args.find)
-	occurences += count
-	start = 0
-	for i in range(count):
-		start = contents.find(args.find,start)
-		end = start+len(args.find)
-		contents = contents[:start]+args.replace+contents[end:]
-		start = start+len(args.replace)		#begin next pass right after what was added
-		
-	f.close()
-	g = open(textfile,'w')
-	g.write(contents)
-	g.close()
-print "Replaced " + str(occurences) + " occurences of " + args.find + " in " + args.source
+    with open(textfile, 'r') as f:
+        content, count = re.subn(args.find, args.replace, f.read())
+
+    with open(textfile, 'w') as f:
+        f.write(content)
+
+print "Replaced {} occurences of {} in {}".format(
+    str(count), args.find, args.source)
